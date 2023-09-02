b58021798fb94f3f87e15051b288ac1f9fbaf814
Cristiann-Paredes <paredes30@hotmail.es>
2023-09-01 20:20:49 -0500

    model

--- src/main.java
+++ src/main.java
@@ -1,2 +1,18 @@
+import controlador.CtrGestionCajeros;
+import controlador.CtrLogin;
+import controlador.CtrProductosAdmin;
+import modelo.DatosCompartidos;
+import vistas.VistaCrudCajeroAdmi;
+import vistas.VistaCrudProductosAdmi;
+import vistas.VistaFacturaCajero;
+import vistas.VistaLogin;
+import controlador.*;
+
 public class main {
+    public static void main(String[] args) {
+        DatosCompartidos dc = new DatosCompartidos();
+        VistaLogin vl = new VistaLogin();
+        new CtrLogin(vl, dc);
+        vl.setVisible(true);
+    }
 }
